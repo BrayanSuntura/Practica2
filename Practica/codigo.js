e7dc8fb9c6e156f479d296394fb4bd6cb6230162
BrayanSunturaQuenta <161734221+BrayanSuntura@users.noreply.github.com>
2024-03-13 23:25:22 -0400

    Add files via upload

---
+++ Practica/codigo.js
@@ -0,0 +1,12 @@
+document.getElementById('facebook').addEventListener('click', function() {
+    document.getElementById('facebook-details').style.display = 'block';
+  });
+  
+  document.getElementById('twitter').addEventListener('click', function() {
+    document.getElementById('twitter-details').style.display = 'block';
+  });
+  
+  document.getElementById('instagram').addEventListener('click', function() {
+    document.getElementById('instagram-details').style.display = 'block';
+  });
+  
